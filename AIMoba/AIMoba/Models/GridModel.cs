636b4193d05f8a7b4b47a19081cd7fca79eb4334
Kristof <kristofosztopani@gmail.com>
2019-10-13 07:42:59 +0200

    Grid letrehozasa a Models mappaba.

---
+++ AIMoba/AIMoba/Models/GridModel.cs
@@ -0,0 +1,83 @@
+
+namespace AIMoba.Models
+{
+    // Maximum 4 játékos támogatott
+    public enum FieldState
+    {
+        None, PlayerOne, PlayerTwo, PlayerThree, PlayerFour
+    }
+
+    public class GridModel
+    {
+        // Az tömb szélessége és magassága lekérhetõ, de csak külön metodus során állítható
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        // Az amõba tábla
+        private FieldState[,] fields;
+
+        public GridModel(int width = 20, int height = 20)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.fields = new FieldState[Height, Width];
+            ResetCellsToDefault();
+        }
+
+        // a Grid classból a tábla x, y helyen lévõ eleme megkaphatók
+        // (nem változtatható) [x,y] színtaktikával
+        /* például:
+            Grid testGrid = new Grid(); testGrid[2,2]==FieldState.PlayerOne
+        */
+        public FieldState this[int iPos, int jPos]
+        {
+            get
+            {
+                return fields[iPos, jPos];
+            }
+        }
+
+        // hamis értékkel tér vissza ha szabálytalan a lépés
+        // TODO: IsValidMove metódust implementálni
+        public bool MakeMove(int iPos, int jPos, FieldState _fieldState)
+        {
+            if (IsValidMove(iPos, jPos))
+            {
+                fields[iPos, jPos] = _fieldState;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void SetSize(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.fields = new FieldState[Width, Height];
+            ResetCellsToDefault();
+        }
+
+        // Minden mezõt semlegesre állít(egy játékoshoz sem tartozik)
+        private void ResetCellsToDefault()
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    this.fields[i, j] = FieldState.None;
+                }
+            }
+        }
+
+        private bool IsValidMove(int iPos, int jPos)
+        {
+
+            return true;
+        }
+
+    }
+
+}
